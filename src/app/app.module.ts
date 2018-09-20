b597dd8947f7a3fdf1fb396a2645c0d2f74707db
Ivan Ivanov <isipro@gmail.com>
2018-09-20 16:00:13 +0300

    Task: Create html sections and components: header, navigation, sports, footer - grid and some styling ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ServicesComponent } from './services/services.component';
+import { FooterComponent } from './footer/footer.component';
+import { SportsComponent } from './sports/sports.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent,
+    ServicesComponent,
+    FooterComponent,
+    SportsComponent
   ],
   imports: [
     BrowserModule,
