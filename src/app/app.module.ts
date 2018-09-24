2510a8eafacd3d8367298b0ccd98fcaf4e1d290f
Ivan Ivanov <isipro@gmail.com>
2018-09-24 15:14:10 +0300

    TASKS: contact section, correct styling, fix menu, upload to dev.premier-camps.com

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { AppRoutingModule } from './/app-routing.module';
+//import { AppRoutingModule } from './/app-routing.module';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ServicesComponent } from './services/services.component';
 import { FooterComponent } from './footer/footer.component';
@@ -22,7 +22,7 @@ import { FacilitiesComponent } from './facilities/facilities.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    //AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
