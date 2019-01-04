b4c92e3e99ed4a5155eb956a010716ee239f6d65
Ivan Ivanov <isipro@gmail.com>
2019-01-04 17:03:20 +0200

    edited/compl services and completed home intro message

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -7,6 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServicesComponent implements OnInit {
 
+
+  consultancy = [
+    'Coaching Coaches to improve individual and team performance',
+    'Elite athlete coaching and tournament preparation',
+    'Specialist one on one coaching',
+    'Training camp planning and assessment',
+    'Remedial training and technique improvement',
+    'Training program design, planning and delivery', 
+    'Nutrition, conditioning and strength',
+    'Sports Psychology',
+    'Transportation planning and logistics',
+    'International Tournament and Event Planning, Implementation and Staging',
+    'Advising and liaising with National Federations on Athlete development',
+    'Strategic development plans for Junior athletes'
+  ]
+
   constructor() { }
 
   ngOnInit() {
